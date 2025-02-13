58c62811b0a0a917214b8c0d4a063cca636f5cbc
Andrey Salnikov <andrycalnicow@gmail.com>
2025-02-13 18:11:46 +0400

    Add check scripts for strings_clone_kata

---
+++ strings_kata/clone_kata/good/clone_kata_good.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+	"strings"
+)
+
+func Ttr() string {
+	str := strings.Repeat("A", 10*1024*1024) // Repeat "A" n times
+	b := strings.Clone(str[100:200])         // We create a new string
+	fmt.Println("Created a large variable")
+	return b
+}
+
+func main() {
+	var m runtime.MemStats
+
+	// Run the function
+	b := Ttr()
+
+	// Check memory usage before GC
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Before GC: %v MB\n", m.Alloc/1024/1024)
+
+	// Run garbage collector and free memory in OS
+	runtime.GC()
+	debug.FreeOSMemory()
+
+	// Check memory usage after GC
+	runtime.ReadMemStats(&m)
+	fmt.Printf("After GC: %v MB\n", m.Alloc/1024/1024)
+
+	fmt.Println(b)
+}
